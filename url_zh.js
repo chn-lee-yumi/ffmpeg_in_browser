f441ec1a605d3eef24aaf78d2b9d4a8b2b37240a
liyumin <chn.lee.yumi@gmail.com>
2023-12-29 21:41:47 +1100

    update url_zh.js

--- url_zh.js
+++ url_zh.js
@@ -1,3 +1,5 @@
-const baseURLFFMPEG = 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/umd';
-const baseURLCore = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd';
+const baseURLFFMPEG = 'ffmpeg/ffmpeg@0.12.10';
+const baseURLCore = 'ffmpeg/core@0.12.6';
+//const baseURLFFMPEG = 'https://cdn.jsdelivr.net/npm/@ffmpeg/ffmpeg@0.12.10/dist/umd';
+//const baseURLCore = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.6/dist/umd';
 const baseURLCoreMT = 'https://cdn.jsdelivr.net/npm/@ffmpeg/core-mt@0.12.6/dist/umd';
